ebf4d9f3695e41200590cc7920d8a07943aa0e7f
Connor Bode <connor.bode@gmail.com>
2020-06-14 14:39:36 -0400

    v1.0.2

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ echo "Current version is $(cat VERSION.txt).  What version did you want to relea
 read VERSION
 echo $VERSION > VERSION.txt
 python setup.py sdist
-twine upload "dist/django_fixed_timezone_field-$VERSION.tar.gz"
+twine upload "dist/django-fixed-timezone-field-$VERSION.tar.gz"
 git add .
 git commit -m "v$VERSION"
 git tag -a "v$VERSION" -m "v$VERSION"
